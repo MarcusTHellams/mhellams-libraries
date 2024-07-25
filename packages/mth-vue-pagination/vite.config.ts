4b59d644b38bbe46d6d57a67c652b0ecf53cbaf1
mhellams <mhellams@collegeboard.org>
2024-07-25 10:01:24 -0400

    update types

--- packages/mth-vue-pagination/vite.config.ts
+++ packages/mth-vue-pagination/vite.config.ts
@@ -1,29 +1,28 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import pkg from './package.json';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import pkg from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), dts()],
-	server: {
-		open: true,
-	},
-	build: {
-		minify: 'esbuild',
-		lib: {
-			entry: './lib/index.ts',
-			formats: ['cjs', 'es'],
-			fileName: (format) => `${pkg.name.split('/')[1]}-${format}.js`,
-		},
-		sourcemap: true,
-		rollupOptions: {
-			external: [...Object.keys(pkg.peerDependencies)],
-			output: {
-				globals: {
-					vue: 'Vue',
-				},
-			},
-		},
-	},
+  plugins: [vue()],
+  server: {
+    open: true,
+  },
+  build: {
+    minify: "esbuild",
+    lib: {
+      entry: "./lib/index.ts",
+      formats: ["cjs", "es"],
+      fileName: (format) => `${pkg.name.split("/")[1]}-${format}.js`,
+    },
+    sourcemap: true,
+    rollupOptions: {
+      external: [...Object.keys(pkg.peerDependencies)],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 });
